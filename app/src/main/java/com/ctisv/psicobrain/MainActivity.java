eaff623d118938e782d6fb520de4448054656bd2
fernando lopez <fernandolopezm22@gmail.com>
2020-03-18 12:46:56 -0600

    CAMBIO ACTIVITYS

--- app/src/main/java/com/ctisv/psicobrain/MainActivity.java
+++ app/src/main/java/com/ctisv/psicobrain/MainActivity.java
@@ -1,14 +1,44 @@
 package com.ctisv.psicobrain;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnlogin;
+    TextView tvGoSingup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnlogin = findViewById(R.id.btnLogin);
+        tvGoSingup = findViewById(R.id.TextViewGoSingup);
+
+
+        btnlogin .setOnClickListener(this);
+        tvGoSingup.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        switch (id){
+            case R.id.btnLogin:
+                break;
+            case R.id.TextViewGoSingup:
+                goToSignup();
+                break;
+        }
+    }
+
+    private void goToSignup() {
+        Intent i = new Intent(MainActivity.this, SingupActivity.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/ctisv/psicobrain/SingupActivity.java
+++ app/src/main/java/com/ctisv/psicobrain/SingupActivity.java
@@ -1,14 +1,26 @@
 package com.ctisv.psicobrain;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-public class SingupActivity extends AppCompatActivity {
+public class SingupActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnSingup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_singup);
+
+        btnSingup = findViewById(R.id.btnLogin);
+        btnSingup.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+
     }
 }
